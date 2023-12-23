9233a5c953003e9136b22d1291ff68ff471d960a
Abdelrazik Ehab <92217502+AbdelrazikEhab@users.noreply.github.com>
2023-12-23 17:35:40 +0200

    ...
    ....

--- index.js
+++ index.js
@@ -1,116 +1,73 @@
 const express = require("express");
-const axios = require("axios");
+const TradingView = require("tradingview-api-adapter");
+const zmq = require("zeromq");
+
 const app = express();
 const port = 3000;
 
-// Import the zeromq.js library
-const zmq = require("zeromq");
+// Create ZeroMQ socket
+const zmqSocket = zmq.socket("push");
+const endpoint = "tcp://127.0.0.1:5555"; // Replace with the actual ZeroMQ endpoint
+console.log(Replace);
 
-// Call sell and buy
-const performBuyOperation = require("./performBuyOperation");
-const performSellOperation = require("./performSellOperation");
-
-// Function to fetch indicators from TradingView API
-async function fetchIndicators() {
+// Function to fetch indicators from TradingView using tradingview-api-adapter
+async function fetchIndicators(symbol, exchange) {
   try {
-    const symbol = "AAPL"; // Replace with the symbol or trading pair you are interested in
-    const resolution = "D"; // Replace with the desired resolution (e.g., D for daily, 1H for hourly, etc.)
+    const adapter = new TradingView();
 
-    const apiKey = "YOUR_API_KEY"; // Replace with your TradingView API key
+    adapter
+      .Quote(`${exchange}:${symbol}`, ["lp", "ch", "chp"])
+      .listen((data) => {
+        console.log("Last price: ", data.lp);
+        console.log("Price change: ", data.ch);
+        console.log("Price change in percent: ", data.chp);
 
-    const endpoint = `https://api.example.com/indicators?symbol=${symbol}&resolution=${resolution}&apikey=${apiKey}`;
-    // Process the response and extract indicator values
-    const indicators = endpoint.data;
-    // Implement your logic to extract specific indicator values from the response
-
-    // Perform trading operations based on indicators
-    performTradingOperations(indicators);
+        // Perform trading operations and send response to MQL5
+        const decision = makeTradingDecision(data);
+        sendResponseToMQL5(decision);
+      });
   } catch (error) {
-    console.error(
-      "Error fetching indicators from TradingView API:",
-      error.message
-    );
+    console.error("Error fetching indicators from TradingView:", error.message);
   }
 }
 
-// Function to perform trading operations based on indicators
-function performTradingOperations(indicators) {
-  // Implement your trading strategy based on the indicators
-  // Generate buy/sell signals and execute orders accordingly
+// Function to make trading decision based on indicator data
+function makeTradingDecision(data) {
+  const lastPrice = data.lp;
+  const priceChange = data.ch;
+  const priceChangePercent = data.chp;
 
-  // Example trading strategy: Buy when indicator value is above a certain threshold, sell when below
-  const indicatorValue = indicators.someIndicator; // Replace with the actual indicator value based on your data structure
-  const buyThreshold = 0.8; // Replace with your desired buy threshold value
-  const sellThreshold = 0.2; // Replace with your desired sell threshold value
-
-  if (indicatorValue > buyThreshold) {
-    // Buy signal
-    console.log("Buy signal detected");
-    performBuyOperation(symbol, volume); // Replace with the appropriate symbol and volume
-  } else if (indicatorValue < sellThreshold) {
-    // Sell signal
-    console.log("Sell signal detected");
-    performSellOperation(symbol, volume); // Replace with the appropriate symbol and volume
+  // Implement your trading strategy based on the indicator data
+  if (priceChange > 0 && priceChangePercent > 0) {
+    return "buy";
+  } else if (priceChange < 0 && priceChangePercent < 0) {
+    return "sell";
+  } else {
+    return "hold";
   }
 }
 
-// API endpoint for receiving requests from MetaTrader
-app.get("/api/placeOrder", async (req, res) => {
-  // Extract parameters from the request
-  const symbol = req.query.symbol;
-  const volume = req.query.volume;
-  const orderType = req.query.orderType;
+// Function to send trading decision response to MQL5 using ZeroMQ
+function sendResponseToMQL5(decision) {
+  zmqSocket.send(decision);
+  console.log("Response sent to MQL5:", decision);
+}
 
-  // Validate the order parameters
-  if (!symbol || !volume || !orderType) {
-    res.status(400).send("Missing required parameters");
+// API endpoint for fetching indicators and making trading decision
+app.post("/trading-decision/:symbol/:exchange", (req, res) => {
+  const symbol = req.params.symbol;
+  const exchange = req.params.exchange;
+
+  if (!symbol || !exchange) {
+    res.status(400).send("Symbol and exchange are required.");
     return;
   }
 
-  // ZeroMQ socket for sending commands to MetaTrader
-  const socket = new zmq.Request();
-
-  try {
-    // Connect the socket to the MetaTrader ZeroMQ server
-    await socket.connect("tcp://127.0.0.1:5555");
-
-    // Construct the request message
-    const request = {
-      command: "placeOrder",
-      symbol: symbol,
-      volume: volume,
-      orderType: orderType,
-    };
-
-    // Send the request to MetaTrader
-    await socket.send(JSON.stringify(request));
-
-    // Receive the response from MetaTrader
-    const [response] = await socket.receive();
-
-    // Process the response
-    const parsedResponse = JSON.parse(response.toString());
-
-    if (parsedResponse.success) {
-      console.log(`Order placed successfully: ${parsedResponse.message}`);
-      res.send("Order placed successfully");
-    } else {
-      console.log(`Failed to place order: ${parsedResponse.error}`);
-      res.status(500).send("Failed to place order");
-    }
-  } catch (error) {
-    console.log(`Error occurred while placing order: ${error.message}`);
-    res.status(500).send("Error occurred while placing order");
-  } finally {
-    // Close the socket
-    socket.close();
-  }
+  fetchIndicators(symbol, exchange);
+  res.send("Trading decision process started");
 });
 
-// Start the server
+// Start the Express server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-
-  // Fetch indicators from TradingView API periodically
-  setInterval(fetchIndicators, 5000); // Adjust the interval as needed
+  console.log(`Server is running on port ${port}`);
 });
