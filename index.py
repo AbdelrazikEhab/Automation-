d5ce788a06b0d80d847a254b1cacc83328c95381
Abdelrazik Ehab <92217502+AbdelrazikEhab@users.noreply.github.com>
2023-12-26 16:01:02 +0200

    ....
    ....

--- index.py
+++ index.py
@@ -1,53 +1,73 @@
-import zmq
-from tradingview_ta import TA_Handler, Interval
+import json
+import websocket
+import threading
 
-# Create ZeroMQ socket
-context = zmq.Context()
-zmqSocket = context.socket(zmq.PUSH)
-endpoint = "tcp://127.0.0.1:5555"  # Replace with the actual ZeroMQ endpoint
-zmqSocket.connect(endpoint)
+def on_message(ws, message):
+    print('Received message:', message)
+    data = json.loads(message)
 
-# Function to fetch indicators from TradingView
-def fetch_indicators(symbol, exchange):
-    try:
-        handler = TA_Handler()
-        handler.set_symbol_as(symbol)
-        handler.set_exchange_as_crypto_or_stock(exchange)
-        handler.set_screener_as_crypto()
-        handler.set_interval_as(Interval.INTERVAL_1_DAY)
+    if 'm' in data and 'supertrend' in data['m']:
+        supertrend_data = data['m']['supertrend']
 
-        analysis = handler.get_analysis()
-        last_price = analysis.indicators['close']
-        price_change = analysis.indicators['change']
-        price_change_percent = analysis.indicators['change_percent']
+        for item in supertrend_data:
+            signal_type = item['type']
+            signal_price = item['p']
+            timestamp = item['time']
 
-        # Perform trading operations and send response to MQL5
-        decision = make_trading_decision(price_change, price_change_percent)
-        send_response_to_mql5(decision)
+            if signal_type == 'buy':
+                print(f'Buy signal at {signal_price} (timestamp: {timestamp})')
+            elif signal_type == 'sell':
+                print(f'Sell signal at {signal_price} (timestamp: {timestamp})')
 
-    except Exception as e:
-        print("Error fetching indicators from TradingView:", str(e))
+def on_error(ws, error):
+    print('WebSocket error:', error)
 
-# Function to make trading decision based on indicator data
-def make_trading_decision(price_change, price_change_percent):
-    if price_change > 0 and price_change_percent > 0:
-        return "buy"
-    elif price_change < 0 and price_change_percent < 0:
-        return "sell"
-    else:
-        return "hold"
+def on_close(ws):
+    print('WebSocket connection closed')
 
-# Function to send trading decision response to MQL5 using ZeroMQ
-def send_response_to_mql5(decision):
-    zmqSocket.send_string(decision)
-    print("Response sent to MQL5:", decision)
+# Define the WebSocket URL for the XAU/USD symbol
+symbol = 'XAUUSD'
+url = 'wss://data.tradingview.com/socket.io/websocket?symbol={symbol}'
 
-# Main function to start the automation
-def main():
-    symbol = input("Enter symbol: ")
-    exchange = input("Enter exchange: ")
+# Connect to the WebSocket API
+ws = websocket.WebSocketApp(url,
+                            on_message=on_message,
+                            on_error=on_error,
+                            on_close=on_close)
 
-    fetch_indicators(symbol, exchange)
+# Authenticate with TradingView (replace with your TradingView username and password)
+auth_payload = {
+    'action': 'auth',
+    'params': {
+         'login': 'bitcoin027@gmail.com',
+        'password': 'bitcoin027@123'
+    }
+}
 
-if __name__ == "__main__":
-    main()
+def send_auth_payload():
+    ws.send(json.dumps(auth_payload))
+
+# Subscribe to the Supertrend indicator updates for the XAU/USD symbol
+subscribe_payload = {
+    'action': 'subscribe',
+    'params': {
+        'symbol': symbol,
+        'interval': '1',  # Time interval (1 minute)
+        'supertrend': 'true'
+    }
+}
+
+def send_subscribe_payload():
+    ws.send(json.dumps(subscribe_payload))
+
+# Start a separate thread to run the WebSocket connection
+ws_thread = threading.Thread(target=ws.run_forever)
+ws_thread.start()
+
+# Authenticate and subscribe after a short delay to ensure the connection is established
+# Adjust the delay time as needed (in seconds)
+auth_timer = threading.Timer(5, send_auth_payload)
+subscribe_timer = threading.Timer(7, send_subscribe_payload)
+
+auth_timer.start()
+subscribe_timer.start()
